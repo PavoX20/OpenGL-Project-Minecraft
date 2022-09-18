37ebc367bfa3916552190e82f6a4ba7d5bba481a
Pablo Sarzosa <pablo.sarzosa@epn.edu.ec>
2022-09-18 08:37:03 -0500

    chore: Initial commit | feat: Project is completed

---
+++ OpenGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGL
+//
+//  Created by Pablo Sarzosa on 17/2/22.
+//
+
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
